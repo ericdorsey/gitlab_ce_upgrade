68f4d1367383465b79a82bc1ab3cd90192320968
Eric Dorsey <dorseye@gmail.com>
2019-06-26 20:50:38 -0600

    Add optional specific version install via argument

--- updategitlab.sh
+++ updategitlab.sh
@@ -1,9 +1,30 @@
 #!/bin/bash
 
+# Usage:
+# If script is run without an argument it attempts to install whatever the latest gitlab-ce it finds
+# If script is run with an arugment it attempts to install the version supplied (as the first argument)
+
+# Check if running as root
 if [[ $EUID -ne 0 ]]; then
    echo "Exiting. This script must be run as root"
    exit 1
 fi
+
+echo "INSTRUCTIONS:"
+echo "If receive message 'It is recommended to upgrade to the last minor version in a major version series firste before jumping to the next major version'"
+echo "Find version number with:"
+echo "# apt list --all-versions gitlab-ce | head -n 20"
+echo "Install that version"
+echo "# apt-get install gitlab-ce=version_number_here"
+echo ""
+
+# Example "next major version" message
+#gitlab preinstall: It seems you are upgrading from 11.x version series
+#gitlab preinstall: to 12.x series. It is recommended to upgrade
+#gitlab preinstall: to the last minor version in a major version series first before
+#gitlab preinstall: jumping to the next major version.
+#gitlab preinstall: Please follow the upgrade documentation at https://docs.gitlab.com/ee/policy/maintenance.html#upgrade-recommendations
+#gitlab preinstall: and upgrade to 11.11 first.
 
 # Get the date
 mydate=$(date +'%Y-%m-%d_%H-%M-%S')
@@ -18,5 +39,18 @@ mkdir -p $outputlogdir
 # Create fullfilename
 fullfilename=$outputlogdir$filename
 
-# Make a backup of gitlab before upgrading
-gitlab-rake gitlab:backup:create STRATEGY=copy && apt-get update && apt-get install gitlab-ce 2>&1 | tee $fullfilename
+# Output Current Env. & Version Info to the Log File
+echo "Running gitlab-rake gitlab:env:info" | tee $fullfilename
+gitlab-rake gitlab:env:info 2>&1 | tee $fullfilename
+
+# Check if arguments passed to script was zero
+if [ "$#" -eq "0" ]
+then
+    # Make a backup of gitlab before upgrading
+    echo "Running gitlab-rake gitlab:backup:create STRATEGY=copy && apt-get update && apt-get install gitlab-ce" 2>&1 | tee $fullfilename
+    gitlab-rake gitlab:backup:create STRATEGY=copy && apt-get update && apt-get install gitlab-ce 2>&1 | tee $fullfilename
+else
+    # Upgrade to the version number passed as the first argument
+    echo "Running gitlab-rake gitlab:backup:create STRATEGY=copy && apt-get update && apt-get install gitlab-ce=$1" 2>&1 | tee $fullfilename
+    gitlab-rake gitlab:backup:create STRATEGY=copy && apt-get update && apt-get install gitlab-ce=$1 | tee $fullfilename
+fi
